3cc683a8e4a2c12dba93a85eabe9670f0e554564
Claiirou <clairemachet@hotmail.fr>
2022-07-20 15:52:41 +0200

    add pseudo on input

--- components/TweetBox.js
+++ components/TweetBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function TweetBox() {
   return (
-    <div className="flex space-x-2 p-5 bg-white rounded-3xl mt-2 mx-12 md:mx-32">
+    <div className="flex  space-x-2 p-5 bg-white rounded-3xl mt-2 mx-12 md:mx-32">
       <img
         className="h-14 w-14 mr-5 object-cover rounded-full mt-4"
         src="/avatar.jpg"
@@ -10,11 +10,19 @@ function TweetBox() {
       />
       <div className="flex flex-1 items-center pl-2">
         <form className="flex flex-1 flex-col">
-          <input
-            className="h-24 w-full text-xl outline-none placeholder:text-xl"
-            type="text"
-            placeholder="What's up today ?"
-          ></input>
+          <div className="flex">
+            <input
+              className="h-24 w-1/4 text-xl text-black outline-none placeholder:text-xl"
+              type="text"
+              placeholder="Pseudo ?"
+            ></input>
+            <input
+              className="h-24 w-full text-xl text-black outline-none placeholder:text-xl"
+              type="text"
+              placeholder="What's up today ?"
+            ></input>
+          </div>
+
           <div>
             <button className="bg-[#001021] text-white font-bold px-5 py-2 rounded-full  ">
               Tweet
---
+++ prisma/migrations/20220720134631_modify_date/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Tweet` MODIFY `date` VARCHAR(191) NULL;
