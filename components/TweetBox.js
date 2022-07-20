07beaf50a826068d4cd4a6c6d55cd8266b47fabf
Claiirou <clairemachet@hotmail.fr>
2022-07-20 12:17:14 +0200

    modify size title

--- components/TweetBox.js
+++ components/TweetBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function TweetBox() {
   return (
-    <div className="flex space-x-2 p-5 bg-white rounded-3xl mt-2 mx-32">
+    <div className="flex space-x-2 p-5 bg-white rounded-3xl mt-2 mx-12 md:mx-32">
       <img
         className="h-14 w-14 mr-5 object-cover rounded-full mt-4"
         src="/avatar.jpg"
