de07cd23619b1a92507e5509fa7b563ac7532c65
Claiirou <clairemachet@hotmail.fr>
2022-07-20 12:04:39 +0200

    css and initi of tweet box

---
+++ components/TweetBox.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function TweetBox() {
+  return (
+    <div className="flex space-x-2 p-5 bg-white rounded-3xl mt-2 mx-32">
+      <img
+        className="h-14 w-14 mr-5 object-cover rounded-full mt-4"
+        src="/avatar.jpg"
+        alt="avatar"
+      />
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+            type="text"
+            placeholder="What's up today ?"
+          ></input>
+          <div>
+            <button className="bg-[#001021] text-white font-bold px-5 py-2 rounded-full  ">
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default TweetBox;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Feed from "../components/Feed";
 import Layout from "../components/Layout";
 import style from "../styles/home.module.css";
 import { RefreshIcon } from "@heroicons/react/outline";
+import TweetBox from "../components/TweetBox";
 
 export default function Home() {
   return (
@@ -9,10 +10,11 @@ export default function Home() {
       <div>
         <div className=" mt-20%">
           <h1 className={style.title}>Twitter Lunaire</h1>
-          <div className="flex justify-center">
-            <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-white transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+          <div className="flex justify-center items-center">
+            <RefreshIcon className="my-5 h-8 w-8 cursor-pointer text-white transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
           </div>
         </div>
+        <TweetBox />
         <Feed />
       </div>
     </Layout>
