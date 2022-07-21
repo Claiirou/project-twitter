b50e47da87aa76e475ca1100dcdd7ff0c82b24aa
Claiirou <clairemachet@hotmail.fr>
2022-07-21 15:13:23 +0200

    finish tweet page home

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,19 @@ import { RefreshIcon } from "@heroicons/react/outline";
 import TweetBox from "../components/TweetBox";
 
 export default function Home() {
+  const handleRefresh = () => {
+    location.reload();
+  };
   return (
     <Layout pageTitle="Bienvenue sur le Twitter Lunaire">
       <div>
         <div className=" mt-20%">
           <h1 className={style.title}>Twitter Lunaire</h1>
           <div className="flex justify-center items-center">
-            <RefreshIcon className="my-5 h-8 w-8 cursor-pointer text-white transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+            <RefreshIcon
+              className="my-5 h-8 w-8 cursor-pointer text-white transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+              onClick={handleRefresh}
+            />
           </div>
         </div>
         <TweetBox />
