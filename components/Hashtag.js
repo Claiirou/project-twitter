c1a14ec25af28dbabc80a712d9a2dd25fd8fba55
Claiirou <clairemachet@hotmail.fr>
2022-07-21 17:32:48 +0200

    css page explorer

--- components/Hashtag.js
+++ components/Hashtag.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hashtag = ({ hashtag }) => {
   return (
     <div className="flex flex-1 flex-col">
-      <div className="flex flex-1  flex-col pl-2 space-x-5 py-4 ml-24 md:ml-[480px]">
+      <div className="border-y-2 w-4/5 flex flex-1 flex-col pl-2 space-x-5 py-4 mx-auto md:w-2/5">
         <div className="text-xl">{hashtag.hashtag}</div>
         <div className="">{hashtag.number}</div>
       </div>
@@ -12,3 +12,5 @@ const Hashtag = ({ hashtag }) => {
 };
 
 export default Hashtag;
+
+// ml-24 md:ml-[480px]
