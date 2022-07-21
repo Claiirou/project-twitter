268a5bb1f1477fbf98d2024c86fce16db166521e
Claiirou <clairemachet@hotmail.fr>
2022-07-21 22:56:45 +0200

    add hover

--- components/Hashtag.js
+++ components/Hashtag.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Hashtag = ({ hashtag }) => {
   return (
-    <div className="flex flex-1 flex-col">
-      <div className="border-y-2 w-4/5 flex flex-1 flex-col pl-2 space-x-5 py-4 mx-auto md:w-2/5">
+    <div className="flex flex-1 flex-col group ">
+      <div className="border-y-2 w-4/5 flex flex-1 flex-col pl-2 space-x-5 py-4 mx-auto group-hover:bg-[#5D6A7E]  md:w-2/5">
         <div className="text-xl">{hashtag.hashtag}</div>
         <div className="">{hashtag.number}</div>
       </div>
@@ -12,5 +12,3 @@ const Hashtag = ({ hashtag }) => {
 };
 
 export default Hashtag;
-
-// ml-24 md:ml-[480px]
